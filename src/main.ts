770114723d2663789914493305830aba8d596de3
yamalpaca <dst.leopard@gmail.com>
2025-10-27 20:38:11 +0000

    add canvas

--- src/main.ts
+++ src/main.ts
@@ -10,25 +10,20 @@ const ctx = canvas.getContext("2d")!;
 
 const tileSize: number = 30;
 
-
 function resizeCanvas() {
-  canvas.width = globalThis.innerWidth-40;
-  canvas.height = (tileSize*8)+50;
+  canvas.width = globalThis.innerWidth - 40;
+  canvas.height = (tileSize * 8) + 50;
 
   if (ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'gray';
+    ctx.fillStyle = "gray";
     for (let i: number = 0; i < 4; i++) {
-      ctx.fillRect(0, tileSize*i*2, canvas.width, tileSize);
+      ctx.fillRect(0, tileSize * i * 2, canvas.width, tileSize);
     }
     ctx.fillRect(0, 240, canvas.width, 50);
   }
-
-
 }
-
 
 resizeCanvas();
 
-
-globalThis.addEventListener('resize', resizeCanvas);
+globalThis.addEventListener("resize", resizeCanvas);
