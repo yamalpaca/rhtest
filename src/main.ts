144074e6a7773e72f5ddc2246724d4cb56fe9878
yamalpaca <dst.leopard@gmail.com>
2025-10-29 00:49:02 +0000

    add row and dividers

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
-import btnIcons from './btnicons.png';
-import './style.css';
+import btnIcons from "./btnicons.png";
+import "./style.css";
 
 document.body.innerHTML = `
 `;
 
-const canvas = document.createElement('canvas');
+const canvas = document.createElement("canvas");
 document.body.append(canvas);
 
-const ctx = canvas.getContext('2d')!;
+const ctx = canvas.getContext("2d")!;
 
 const tileSize: number = 30;
 
 
 
 const rowPal: string[] = [
-  '#FF9D9D',
-  '#FFC668',
-  '#AAFF9B',
-  '#F0BAFF',
-  '#FFE002',
+  "#FF9D9D",
+  "#FFC668",
+  "#AAFF9B",
+  "#F0BAFF",
+  "#FFE002",
 ];
-const inputPal: string = '#141414ff';
+const inputPal: string = "#141414ff";
 
 const myImage = new Image();
 myImage.src = btnIcons;
 
 let selectX: number = -1;
 let selectY: number = -1;
-const selectOffsetX = 86;
+const selectOffsetX = 130;
 
 interface Game {
   inputLength: number;
@@ -38,7 +38,7 @@ interface Game {
 const gameData: Game = {
   inputLength: 19,
   critAmt: 5,
-  critNames: ['A', 'B', 'C', 'D'],
+  critNames: ["A", "B", "C", "D"],
 };
 
 
@@ -46,52 +46,73 @@ function drawCanvas() {
   
 
   canvas.width = globalThis.innerWidth - 40;
-  canvas.height = (tileSize * (gameData.critAmt+1)) + 50;
+  canvas.height = (tileSize * (gameData.critAmt+1)) + 30;
 
   if (!ctx) return;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.lineWidth = 0.5;
+  
 
   for (let i: number = 0; i < gameData.critAmt; i++) {
     
     ctx.fillStyle = rowPal[i];
-    ctx.fillRect(0, tileSize * i, (gameData.inputLength * 30)+selectOffsetX, tileSize);
+    ctx.fillRect(0, tileSize * i, (gameData.inputLength * 30)+selectOffsetX+1, tileSize);
 
-    ctx.fillStyle = 'black';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 0.5;
     ctx.beginPath();
     ctx.moveTo(0, (i * 30) + 29);
-    ctx.lineTo((gameData.inputLength * 30)+selectOffsetX, (i * 30) + 29);
+    ctx.lineTo((gameData.inputLength * 30)+selectOffsetX+1, (i * 30) + 29);
     ctx.stroke();
 
 
-    ctx.fillStyle = 'black';
-    ctx.font = '16px FOT-Seurat Pro';
-    ctx.textAlign = 'right';
+    ctx.fillStyle = "black";
+    ctx.font = "16px FOT-Seurat Pro";
+    ctx.letterSpacing = "1px";
+    ctx.textAlign = "right";
 
     if (i == gameData.critAmt-1) { 
-      ctx.fillText('Skill Star', selectOffsetX-6, (i*30)+20);
+      ctx.fillText("Skill Star", selectOffsetX-8, (i*30)+21);
     } else {
-      ctx.fillText(gameData.critNames[i], selectOffsetX-6, (i*30)+20);
+      ctx.fillText(gameData.critNames[i], selectOffsetX-8, (i*30)+21);
     }
   }
 
   ctx.fillStyle = inputPal;
-  ctx.fillRect(0, tileSize * gameData.critAmt, (gameData.inputLength * 30)+selectOffsetX, 50);
-  ctx.fillStyle = 'white';
-  ctx.fillText('Buttons', selectOffsetX-6, (gameData.critAmt*30)+20);
+  ctx.fillRect(0, tileSize * gameData.critAmt, (gameData.inputLength * 30)+selectOffsetX+1, 60);
+  ctx.fillStyle = "white";
+  ctx.fillText("Buttons", selectOffsetX-8, (gameData.critAmt*30)+21);
+  ctx.fillText("Timing", selectOffsetX-8, (gameData.critAmt*30)+51);
+
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 1;
+  ctx.beginPath();
+  ctx.moveTo(selectOffsetX-2, 0);
+  ctx.lineTo(selectOffsetX-2, gameData.critAmt*30);
+  ctx.stroke();
+  ctx.strokeStyle = 'white';
+  ctx.beginPath();
+  ctx.moveTo(selectOffsetX-2, gameData.critAmt*30);
+  ctx.lineTo(selectOffsetX-2, (gameData.critAmt*30)+60);
+  ctx.stroke();
 
 
 
   for (let i: number = 0; i < gameData.inputLength; i++){
     if (selectX == i && selectY == gameData.critAmt) {
-      ctx.filter = 'brightness(175%)';
+      ctx.filter = "brightness(175%)";
     } else {
-      ctx.filter = 'brightness(100%)';
+      ctx.filter = "brightness(100%)";
     }
     drawButton((i*tileSize)+selectOffsetX, gameData.critAmt * tileSize, 1);
+    if (selectX == i && selectY == gameData.critAmt+1) {
+      ctx.filter = "brightness(175%)";
+    } else {
+      ctx.filter = "brightness(100%)";
+    }
+    drawButton((i*tileSize)+selectOffsetX-1, (gameData.critAmt+1) * tileSize, 2);
   }
-  ctx.filter = 'none';
+  ctx.filter = "none";
 
 
   
@@ -100,15 +121,15 @@ function drawCanvas() {
 
 myImage.onload = () => drawCanvas();
 
-globalThis.addEventListener('resize', drawCanvas);
+globalThis.addEventListener("resize", drawCanvas);
 
-canvas.addEventListener('mousemove', (e) => {
+canvas.addEventListener("mousemove", (e) => {
   selectX = Math.floor((e.offsetX-selectOffsetX) / 30);
   selectY = Math.floor(e.offsetY / 30);
   drawCanvas();
 });
 
-canvas.addEventListener('mouseleave', () => {
+canvas.addEventListener("mouseleave", () => {
   selectX = -1;
   selectY = -1;
   drawCanvas();
