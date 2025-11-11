211267425ed46cbd41fab8090c2c99776c21502e
yamalpaca <dst.leopard@gmail.com>
2025-11-11 01:05:48 +0000

    revise data text

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,23 @@ import meterimg from "./meterimg.png";
 import pntimg from "./pntimg.png";
 import "./style.css";
 
+interface Criteria {
+  id: number;
+  name: string;
+  weight: number;
+  hit: number;
+  total: number;
+  score: number;
+}
+
+const critPal: string[] = [
+  "#ABC8FF",
+  "#AAFF9B",
+  "#FF9D9D",
+  "#F0BAFF",
+  "#FFE002",
+];
+
 interface Input {
   criteria: number;
   button: number;
@@ -18,14 +35,16 @@ const inputDefault: Input = {
 };
 
 interface Game {
-  criterias: string[];
-  critWeight: number[];
+  critid: number[];
+  critname: string[];
+  critweight: number[];
   inputs: Input[];
 }
 
 const gameData: Game = {
-  criterias: ["Normal", "Triple Punch"],
-  critWeight: [65, 25, 10],
+  critid: [0, 1, 4],
+  critname: ["Main", "Triple Punch"],
+  critweight: [65, 25, 10],
   inputs: [
     ...Array(13).fill({ ...inputDefault }),
     { ...inputDefault, criteria: 2 },
@@ -39,32 +58,28 @@ const chartCanvas = document.createElement("canvas");
 document.body.append(chartCanvas);
 
 const pressText = document.createElement("p");
-pressText.innerHTML = `Presses: <span id="pressCounter"/>`;
-pressText.style.fontFamily = "FOT-Seurat Pro";
+pressText.style.fontFamily = "Arial";
 document.body.append(pressText);
-const pressCounter = document.getElementById("pressCounter");
+let pressCounter: number;
+
+const diffText = document.createElement("p");
+diffText.style.fontFamily = "Arial";
+document.body.append(diffText);
 
 const meterCanvas = document.createElement("canvas");
 document.body.append(meterCanvas);
 
 const scoreText = document.createElement("p");
-scoreText.innerHTML = `<span id="finalScore"/>`;
+scoreText.textContent = "";
 scoreText.style.fontFamily = "Kurokane";
 document.body.append(scoreText);
-const finalScore = document.getElementById("finalScore");
+let finalScore: number;
+let prevScore: number;
 
 let dataTable = document.createElement("table");
 document.body.append(dataTable);
 
 const tileSize: number = 30;
-
-const rowPal: string[] = [
-  "#ABC8FF",
-  "#AAFF9B",
-  "#FF9D9D",
-  "#F0BAFF",
-  "#FFE002",
-];
 
 const btnIcons = new Image();
 btnIcons.src = btnimg;
@@ -77,20 +92,56 @@ let selectX: number = -1;
 let selectY: number = -1;
 const selectOffsetX = 90;
 let mouseFocus: number = 0;
+const currCrit: Criteria[] = [];
 
 const btnPressed: boolean[] = Array(gameData.inputs.length).fill(true);
 let drawState: boolean;
 
 function updatePage() {
+  updateData();
   drawChartCanvas();
   updateText();
   drawMeterCanvas();
 }
 
+function updateData() {
+  currCrit.splice(0, currCrit.length);
+
+  prevScore = finalScore;
+
+  finalScore = 0;
+
+  pressCounter = 0;
+
+  for (let i: number = 0; i < gameData.critid.length; i++) {
+    currCrit.push({
+      id: gameData.critid[i],
+      name: gameData.critid[i] == 4 ? "Skill Star" : gameData.critname[i],
+      weight: gameData.critweight[i],
+      hit: 0,
+      total: 0,
+      score: 0,
+    });
+  }
+
+  for (let i: number = 0; i < gameData.inputs.length; i++) {
+    currCrit[gameData.inputs[i].criteria].total++;
+    if (btnPressed[i]) {
+      pressCounter++;
+      currCrit[gameData.inputs[i].criteria].hit++;
+    }
+  }
+
+  currCrit.forEach((c: Criteria) => {
+    c.score = (c.hit / c.total) * c.weight;
+    finalScore += c.score;
+  });
+}
+
 function drawChartCanvas() {
   //chartCanvas.width = globalThis.innerWidth - 40;
   chartCanvas.width = (gameData.inputs.length * tileSize) + selectOffsetX;
-  chartCanvas.height = (tileSize * (gameData.criterias.length + 3)) + 30;
+  chartCanvas.height = tileSize * (currCrit.length + 3);
 
   const ctx = chartCanvas.getContext("2d")!;
   if (!ctx) return;
@@ -103,12 +154,11 @@ function drawChartCanvas() {
   ctx.lineWidth = 0.5;
   ctx.beginPath();
   ctx.moveTo(selectOffsetX - 1, 0);
-  ctx.lineTo(selectOffsetX - 1, (gameData.criterias.length + 4) * tileSize);
+  ctx.lineTo(selectOffsetX - 1, (currCrit.length + 3) * tileSize);
   ctx.stroke();
 
-  for (let i: number = 0; i < gameData.criterias.length + 1; i++) {
-    ctx.fillStyle = rowPal[i];
-    if (i == gameData.criterias.length) ctx.fillStyle = rowPal[4];
+  for (let i: number = 0; i < currCrit.length; i++) {
+    ctx.fillStyle = critPal[currCrit[i].id];
     ctx.fillRect(
       0,
       tileSize * (i + 1),
@@ -119,10 +169,10 @@ function drawChartCanvas() {
     ctx.strokeStyle = "black";
     ctx.lineWidth = 0.5;
     ctx.beginPath();
-    ctx.moveTo(0, (i * 30) + 59);
+    ctx.moveTo(0, (i + 2) * tileSize - 1);
     ctx.lineTo(
-      (gameData.inputs.length * 30) + selectOffsetX,
-      (i * 30) + 59,
+      (gameData.inputs.length * tileSize) + selectOffsetX,
+      (i + 2) * tileSize - 1,
     );
     ctx.stroke();
 
@@ -132,30 +182,24 @@ function drawChartCanvas() {
 
     ctx.fillStyle = "black";
     ctx.font = "12px Arial";
-    ctx.fillText("999/999", selectOffsetX - 32, (i + 2) * tileSize - 10);
+    ctx.fillText(
+      currCrit[i].hit.toString() + "/" + currCrit[i].total.toString(),
+      selectOffsetX - 32,
+      (i + 2) * tileSize - 10,
+    );
 
-    if (i == gameData.criterias.length) {
-      drawIcon(
-        selectOffsetX - tileSize - 1,
-        (i + 1) * tileSize,
-        0,
-        4,
-        pntIcons,
-        chartCanvas,
-      );
-    } else {
-      drawIcon(
-        selectOffsetX - tileSize - 1,
-        (i + 1) * tileSize,
-        0,
-        i,
-        pntIcons,
-        chartCanvas,
-      );
-    }
+    drawIcon(
+      selectOffsetX - tileSize - 1,
+      (i + 1) * tileSize,
+      0,
+      currCrit[i].id,
+      pntIcons,
+      chartCanvas,
+    );
   }
 
   ctx.font = "16px FOT-Seurat Pro";
+  ctx.letterSpacing = "1px";
   ctx.fillStyle = "white";
   ctx.fillText(
     "Inputs",
@@ -165,48 +209,48 @@ function drawChartCanvas() {
   ctx.fillText(
     "Buttons",
     selectOffsetX - 7,
-    ((gameData.criterias.length + 2) * 30) + 21,
+    (currCrit.length + 2) * tileSize - 9,
   );
   ctx.fillText(
     "Timing",
     selectOffsetX - 7,
-    ((gameData.criterias.length + 2) * 30) + 51,
+    (currCrit.length + 3) * tileSize - 9,
   );
 
   ctx.strokeStyle = "black";
   ctx.lineWidth = 1;
   ctx.beginPath();
   ctx.moveTo(selectOffsetX - 1, tileSize);
-  ctx.lineTo(selectOffsetX - 1, (gameData.criterias.length + 2) * tileSize);
+  ctx.lineTo(selectOffsetX - 1, (currCrit.length + 1) * tileSize);
   ctx.stroke();
 
   if (selectX > -1 && selectX < gameData.inputs.length) {
     ctx.fillStyle = "#ffffff30";
     ctx.fillRect(
       (selectX * tileSize) + selectOffsetX,
-      tileSize * (gameData.criterias.length + 2),
+      tileSize * (currCrit.length + 1),
       tileSize,
-      60,
+      tileSize * 2,
     );
   }
 
   for (let i: number = 0; i < gameData.inputs.length; i++) {
     ctx.filter = "brightness(100%)";
-    let pntindex = gameData.inputs[i].criteria;
-    if (pntindex == gameData.criterias.length) pntindex = 4;
-    let pnttype = 1;
-    if (!btnPressed[i]) pnttype = 4;
+    const iindex = gameData.inputs[i].criteria == currCrit.length - 1
+      ? 4
+      : gameData.inputs[i].criteria;
+    const itype = btnPressed[i] ? 1 : 4;
 
     drawIcon(
       (i * tileSize) + selectOffsetX,
       (gameData.inputs[i].criteria + 1) * tileSize,
-      pnttype,
-      pntindex,
+      itype,
+      iindex,
       pntIcons,
       chartCanvas,
     );
 
-    if (selectX == i && selectY == gameData.criterias.length + 2) {
+    if (selectX == i && selectY == currCrit.length + 1) {
       ctx.filter = "brightness(150%)";
       if (mouseFocus != 0) ctx.filter = "brightness(75%)";
     } else {
@@ -219,7 +263,7 @@ function drawChartCanvas() {
 
     drawIcon(
       (i * tileSize) + selectOffsetX,
-      (gameData.criterias.length + 2) * tileSize,
+      (currCrit.length + 1) * tileSize,
       gameData.inputs[i].button,
       0,
       btnIcons,
@@ -231,63 +275,51 @@ function drawChartCanvas() {
 
 function drawMeterCanvas() {
   const ctx = meterCanvas.getContext("2d")!;
-  if (!ctx || !finalScore) return;
+  if (!ctx || !scoreText) return;
 
   ctx.fillStyle = "#2A2A2A";
   ctx.fillRect(9, 9, 214, 30);
 
-  if (+finalScore.textContent > 60) {
-    if (+finalScore.textContent > 80) {
+  if (+scoreText.textContent > 60) {
+    if (+scoreText.textContent > 80) {
       ctx.fillStyle = "#FF0000";
-      ctx.fillRect(9, 9, 214 * (+finalScore.textContent / 100), 30);
+      ctx.fillRect(9, 9, 214 * (+scoreText.textContent / 100), 30);
       ctx.fillStyle = "#00BE00";
       ctx.fillRect(9, 9, 214 * 0.8, 30);
     } else {
       ctx.fillStyle = "#00BE00";
-      ctx.fillRect(9, 9, 214 * (+finalScore.textContent / 100), 30);
+      ctx.fillRect(9, 9, 214 * (+scoreText.textContent / 100), 30);
     }
 
     ctx.fillStyle = "#00B4FF";
     ctx.fillRect(9, 9, 214 * 0.6, 30);
   } else {
     ctx.fillStyle = "#00B4FF";
-    ctx.fillRect(9, 9, 214 * (+finalScore.textContent / 100), 30);
+    ctx.fillRect(9, 9, 214 * (+scoreText.textContent / 100), 30);
   }
 
   ctx.drawImage(meter, 0, 0, 232, 48);
 }
 
 function updateText() {
-  if (!finalScore) return;
-  if (!pressCounter) return;
-  const scores: number[] = Array(gameData.criterias.length + 1).fill(0);
-  const totalInputs: number[] = Array(gameData.criterias.length + 1).fill(0);
-  let presses: number = 0;
+  if (!scoreText) return;
+  if (!pressText) return;
+  if (!diffText) return;
 
-  for (let i: number = 0; i < gameData.inputs.length; i++) {
-    const crit = gameData.inputs[i].criteria;
-    totalInputs[crit]++;
-    if (btnPressed[i]) {
-      scores[crit]++;
-      presses++;
-    }
+  scoreText.textContent = Math.floor(badRounding(finalScore)).toString();
+  pressText.textContent = "Presses: " + pressCounter.toString();
+  if (finalScore != prevScore && prevScore !== undefined) {
+    diffText.textContent = finalScore >= prevScore ? "+" : "";
+    diffText.textContent += (badRounding(finalScore - prevScore)).toString();
   }
-
-  let scoreSum: number = 0;
-  for (let i: number = 0; i < gameData.criterias.length + 1; i++) {
-    scoreSum += gameData.critWeight[i] * (scores[i] / totalInputs[i]);
-  }
-
-  finalScore.textContent = Math.floor(badRounding(scoreSum)).toString();
-  pressCounter.textContent = presses.toString();
 
   const temp = document.createElement("table");
   temp.style.borderCollapse = "collapse";
   temp.style.font = "16px FOT-Seurat Pro";
 
-  for (let i = 0; i < gameData.criterias.length + 2; i++) {
+  for (let i = 0; i < currCrit.length + 1; i++) {
     const row = temp.insertRow(-1);
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < 6; j++) {
       const cell = row.insertCell(-1);
       cell.style.border = "1px solid black";
       cell.style.textAlign = "center";
@@ -302,10 +334,9 @@ function updateText() {
           cell.style.textAlign = "right";
           cell.style.width = "125px";
           cell.style.padding = "0px 8px 0px 8px";
-          if (i == 0) text = "criteria";
-          else if (i == gameData.criterias.length + 1) text = "Skill Star";
+          if (i == 0) text = "name";
           else {
-            text = gameData.criterias[i - 1];
+            text = currCrit[i - 1].name;
           }
 
           break;
@@ -313,33 +344,35 @@ function updateText() {
         case 1: {
           if (i == 0) text = "weight";
           else {
-            text = gameData.critWeight[i - 1].toString() + "%";
+            text = currCrit[i - 1].weight + "%";
           }
           break;
         }
         case 2: {
-          if (i == 0) text = "wpi";
+          if (i == 0) text = "hit";
           else {
-            text = badRounding(
-              1 / totalInputs[i - 1] * gameData.critWeight[i - 1],
-            ).toString();
+            text = currCrit[i - 1].hit.toString();
           }
           break;
         }
         case 3: {
-          if (i == 0) text = "score";
+          if (i == 0) text = "miss";
           else {
-            text = scores[i - 1].toString() + " / " +
-              totalInputs[i - 1].toString();
+            text = (currCrit[i - 1].total - currCrit[i - 1].hit).toString();
           }
           break;
         }
         case 4: {
-          if (i == 0) text = "w score";
+          if (i == 0) text = "total";
           else {
-            text = badRounding(
-              scores[i - 1] / totalInputs[i - 1] * gameData.critWeight[i - 1],
-            ).toString();
+            text = currCrit[i - 1].total.toString();
+          }
+          break;
+        }
+        case 5: {
+          if (i == 0) text = "result";
+          else {
+            text = badRounding(currCrit[i - 1].score).toString();
           }
           break;
         }
@@ -354,7 +387,7 @@ function updateText() {
 }
 
 function badRounding(n: number) {
-  return Math.floor(n * 100) / 100;
+  return n > 0 ? Math.floor(n * 100) / 100 : Math.ceil(n * 100) / 100;
 }
 
 btnIcons.onload = () => updatePage();
@@ -366,7 +399,7 @@ globalThis.addEventListener("resize", updatePage);
 chartCanvas.addEventListener("mousemove", (e) => {
   selectX = Math.floor((e.offsetX - selectOffsetX) / 30);
   selectY = Math.floor(e.offsetY / 30);
-  if (mouseFocus == 2 && selectY == gameData.criterias.length + 2) {
+  if (mouseFocus == 2 && selectY == currCrit.length + 1) {
     btnPressed[selectX] = drawState;
   }
   updatePage();
@@ -374,7 +407,7 @@ chartCanvas.addEventListener("mousemove", (e) => {
 
 chartCanvas.addEventListener("mousedown", () => {
   mouseFocus = 1;
-  if (selectY == gameData.criterias.length + 2) {
+  if (selectY == currCrit.length + 1) {
     mouseFocus = 2;
     drawState = !btnPressed[selectX];
     btnPressed[selectX] = !btnPressed[selectX];
